b5035ec41d71c98905db4bacb5055c91d0e7a9a4
Rahul <r27m93@gmail.com>
2022-01-31 11:50:39 +0530

    qu from asbenah

--- README.md
+++ README.md
@@ -511,7 +511,40 @@ Qu. Write a function called stat which calculate mean, median and range of an ar
      stat(numbers).range()
 
 ```Js
- console.log(stat)
+
+ const numbers = [80, 85, 70, 90, 65, 90, 80]
+
+const stat = (num) => {
+  let total = num.reduce((x, y) => x + y)
+  let sortedNum = num.sort()
+
+  return {
+    mean: function () {
+      return total / num.length
+    },
+
+    median: function () {
+      if (sortedNum.length % 2) {
+        return sortedNum[sortedNum.length / 2 - 0.5]
+      } else {
+        return (
+          (sortedNum[sortedNum.length / 3] +
+            sortedNum[sortedNum.length / 3 + 1]) /
+          2
+        )
+      }
+    },
+
+    range: function () {
+      return sortedNum[sortedNum.length - 1] - sortedNum[0]
+    },
+  }
+}
+
+stat(numbers).mean()
+stat(numbers).median()
+stat(numbers).range()
+
 ```
 
 </li>
--- index.js
+++ index.js
@@ -1,53 +1,32 @@
-// 'use strict'
-// Task:
-// The keyboard handler is a function which receives three parameters as input:
+const numbers = [80, 85, 70, 90, 65, 90, 80]
 
-// Key - the entered character on the keyboard.
+const stat = (num) => {
+  let total = num.reduce((x, y) => x + y)
+  let sortedNum = num.sort()
 
-// isCaps (or is_caps) - boolean variable responsible for the enabled 'Caps Lock'. (by default false)
+  return {
+    mean: function () {
+      return total / num.length
+    },
 
-// isShift (or is_shift) - boolean variable which is responsible for whether 'Shift' is pressed. (by default false)
+    median: function () {
+      if (sortedNum.length % 2) {
+        return sortedNum[sortedNum.length / 2 - 0.5]
+      } else {
+        return (
+          (sortedNum[sortedNum.length / 3] +
+            sortedNum[sortedNum.length / 3 + 1]) /
+          2
+        )
+      }
+    },
 
-// Your task to write a function that returns the entered character.
-
-// Requirements for obtaining the 'key' variable:
-// Alphabetical characters must be a lowercase (e.x. 'a', not 'A')
-// It must be a character (e.x '2', not 2 or not [1,2,3])
-// It must be of unit length (e.x. 'a', not 'abc')
-// If the value does not satisfy the condition, return 'KeyError'
-
-// For example:
-
-// handler('a', true) // should return 'A' (because Caps-Lock)
-// handler('1', true) // should return '1' (because Сaps-Lock doesn't work here)
-// handler('a', false, true) // should return 'A' (because Shift)
-// handler('1', false, true) // should return '!'
-
-// handler('')  // should return 'KeyError'
-// handler('A') // should return 'KeyError'
-// handler( 3 ) // should return 'KeyError'
-// handler('abc') // should return 'KeyError'
-// In this kata we use en-US QWERTY layout. (The characters we are working with are shown on a white background)
-
-function handler(key, isCaps = false, isShift = false) {
-  // let newKey = key.toString()
-  let value
-
-  let smallAlph = key.charCodeAt() >= 97 && key.charCodeAt() <= 122
-
-  let capitalAlph = key.charCodeAt() >= 65 && key.charCodeAt() <= 90
-
-  if (smallAlph && isCaps == true && isShift == false) {
-    value = key.toUpperCase()
-  } else if (smallAlph && isCaps == true && isShift == true) {
-    value = key
-  } else if (capitalAlph && isShift == true) {
-    value = key.toLowerCase()
-  } else if (capitalAlph) {
-    value = 'KeyError'
+    range: function () {
+      return sortedNum[sortedNum.length - 1] - sortedNum[0]
+    },
   }
-
-  return value || 'KeyError'
 }
 
-console.log(handler('A', true, false))
+stat(numbers).mean()
+stat(numbers).median()
+stat(numbers).range()

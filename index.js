2daa904c2437d16a287be9cba1dcb0c88f4d7913
Rahul <r27m93@gmail.com>
2022-01-31 12:58:39 +0530

    next qu done

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
-// A junior developer structure student name, skills and
-// score in array of arrays which may not easy to read.
-// Destruction the following array name to name, skills
-// array to skills, scores array to scores, JavaScript score
-// to jsScore and React score to reactScore variable.
+'use strict'
+// Write a function called convertArrayToObject which can convert the array to a structured object.
+//     const students = [
+//         ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]],
+//         ['John', ['HTM', 'CSS', 'JS', 'React'], [85, 80, 85, 80]]
+//       ]
 
-// const student = ['David', ['HTM', 'CSS', 'JS', 'React'],
-//  [98, 85, 90, 95]]
-//   console.log(name, skills, scores)
-//   console.log(jsScore, reactScore)
+//     console.log(convertArrayToObject(students))
+//     [
+//       {
+//         name: 'David',
+//         skills: ['HTM','CSS','JS','React'],
+//         scores: [98,85,90,95]
+//       },
+//       {
+//         name: 'John',
+//         skills: ['HTM','CSS','JS','React'],
+//         scores: [85, 80,85,80]
+//       }
+//     ]
 
-const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
+const students = [
+  ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]],
+  ['John', ['HTM', 'CSS', 'JS', 'React'], [85, 80, 85, 80]],
+]
 
-let [name, skills, scores] = student
-let jsScore = scores[2]
-let reactScore = scores[3]
+const convertArrayToObject = (arr) => {
+  const entries = new Map([
+    ['name', arr[0][0]],
+    ['skills', arr[0][1]],
+    ['scores', arr[0][2]],
+  ])
+
+  const entries1 = new Map([
+    ['name', arr[1][0]],
+    ['skills', arr[1][1]],
+    ['scores', arr[1][2]],
+  ])
+
+  const obj = Object.fromEntries(entries)
+  const obj1 = Object.fromEntries(entries1)
+
+  return [obj, obj1]
+}
+
+console.log(convertArrayToObject(students))
+convertArrayToObject(students)

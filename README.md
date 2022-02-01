c1d49269c02d42ef797a9a5c9411d73173e7b0ae
Rahul <r27m93@gmail.com>
2022-02-01 15:44:48 +0530

    delete later

--- README.md
+++ README.md
@@ -889,4 +889,21 @@ cheker(firstName)
 ```
 
 </li>
+
+<li>
+ Declare an array called shoppingBasket and put at least 5 items you commonly put to your shopping basket.
+
+```Js
+let shoppingBasket = []
+
+shoppingBasket.push('Saamosa', 'chop')
+shoppingBasket.unshift('kela')
+shoppingBasket.push('rosogulla', 'peda', 'barfi', 'ladu')
+shoppingBasket.shift()
+
+console.log(shoppingBasket.indexOf('peda'))
+```
+
+</li>
+
 </ol>
--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-// Declare firstName, lastName, age variables and assign your first name, your last name and your age to the variables.
+// Declare an array called shoppingBasket and put at least 5 items you commonly put to your shopping basket.
 
-// Concatenate the firstName , a space and lastName variable and assigned to a fullName variable.
+let shoppingBasket = []
 
-// Check if your name is long or short. If the length of your first name is greater than 7, you have a long name else you have a short name
+shoppingBasket.push('Saamosa', 'chop')
+shoppingBasket.unshift('kela')
+shoppingBasket.push('rosogulla', 'peda', 'barfi', 'ladu')
+shoppingBasket.shift()
 
-const firstName = 'Pankaj'
-const lastName = 'Dubey'
-let age = 30
-
-let fullName = `${firstName} ${lastName}`
-
-let cheker = (name) => {
-  return name.split('').length >= 7 ? `it is too big` : `it is ok`
-}
-console.log(cheker(firstName))
-cheker(firstName)
+console.log(shoppingBasket.indexOf('peda'))

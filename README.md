7b2cec627aceeae499360b6b179d4bef3ede4487
Rahul <r27m93@gmail.com>
2022-02-01 12:09:23 +0530

    not done will try it later

--- README.md
+++ README.md
@@ -800,6 +800,26 @@ firstWord('a')
 
 ```
 
+```Js
+Will Do it Later
+// Write the function called tenMostSpokenLanaguages, it takes the countriesobject and return the array of objects. The language is key and value is the number of places the language is spoken.
+
+import { objCountries } from './data.js'
+
+// console.log(objCountries)
+
+// for (const property in objCountries) {
+//   console.log(`${property}:${objCountries[property]}`)
+// }
+
+objCountries.forEach((element) => {
+  let lang = element.languages.join()
+  //   lang.push(element.languages)
+  console.log(typeof lang)
+})
+
+```
+
 </li>
 
 </ol>
--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-import { countries } from './data.js'
+// Write the function called tenMostSpokenLanaguages, it takes the countriesobject and return the array of objects. The language is key and value is the number of places the language is spoken.
 
-// console.log(countries)
-//find countries with initial
+import { objCountries } from './data.js'
 
-const firstWord = (initial) => {
-  let caps = initial.toUpperCase()
-  let word = []
-  for (let i = 0; i < countries.length; i++) {
-    if (countries[i].startsWith(caps)) {
-      word.push(countries[i])
-    }
-  }
-  console.log(word)
-}
+// console.log(objCountries)
 
-firstWord('a')
+// for (const property in objCountries) {
+//   console.log(`${property}:${objCountries[property]}`)
+// }
+
+objCountries.forEach((element) => {
+  let lang = element.languages.join()
+  //   lang.push(element.languages)
+  console.log(typeof lang)
+})

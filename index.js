4959c4f5e7c397e74eb99786ed828257e9731786
Rahul <r27m93@gmail.com>
2022-02-02 18:47:07 +0530

    next done

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-let sumOfAll = (n) => {
-  return (n * (n + 1)) / 2
+// eclare an object called person, it has firstName, lastName, age, country, getPersonInfo keys. The getPersonInfo is a method which return about the person object. eg.
+
+//
+
+let person = {
+  firstName: 'Asabeneh',
+  lastName: 'Yetayeh',
+  age: 200,
+  country: 'Finland',
+  getPersonInfo: function () {
+    return `I am ${this.firstName} ${this.lastName}. I am ${this.age} years old. I live in from ${this.country}`
+  },
 }
 
-console.log(sumOfAll(10))
+console.log(person.getPersonInfo())

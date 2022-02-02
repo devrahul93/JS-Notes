98bc96a0631de75a908f60966eb05f14d74da3f6
Rahul <r27m93@gmail.com>
2022-02-02 19:04:13 +0530

    next done

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-// eclare an object called person, it has firstName, lastName, age, country, getPersonInfo keys. The getPersonInfo is a method which return about the person object. eg.
+'use strict'
+let line
 
-//
-
-let person = {
-  firstName: 'Asabeneh',
-  lastName: 'Yetayeh',
-  age: 200,
-  country: 'Finland',
-  getPersonInfo: function () {
-    return `I am ${this.firstName} ${this.lastName}. I am ${this.age} years old. I live in from ${this.country}`
-  },
-}
-
-console.log(person.getPersonInfo())
+for (let line = '#'; line.length < 8; line += '#') console.log(line)

7119b4f19ec6d105e39813b001de7493ea5cee73
Rahul <r27m93@gmail.com>
2022-02-04 16:37:14 +0530

    solved

--- README.md
+++ README.md
@@ -1071,4 +1071,20 @@ console.log(largest)
 ```
 
 </li>
+
+<li>
+Reversing an array Arrays have a reverse method which changes the array by inverting the order in which its elements appear. For this exercise, write a function, reverseArray. The reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order. Without reverse method.
+
+```Js
+const reverseArray = (arr) => {
+  console.log(arr.reverse())
+}
+
+console.log(reverseArray(['a', 'b', 'c']))
+```
+
+</li>
+
+<li>
+</li>
 </ol>
--- index.js
+++ index.js
@@ -1,23 +1,5 @@
-// Maximum Math.max returns its largest argument. We can build something like that now. Write a function findMax that takes three arguments and returns their maxiumum. Without method Math.max method.
-
-// console.log(findMax(0, 10, 5));
-// 10
-// console.log(findMax(0, -10,-2));
-// 0
-
-// const findMax = (...a) => {
-//   let value = Math.max(...a)
-//   console.log(value)
-// }
-
-// console.log(findMax(0, 10, -2))
-
-var arr = [0, 10, 1]
-var largest = arr[0]
-
-for (var i = 0; i < arr.length; i++) {
-  if (largest < arr[i]) {
-    largest = arr[i]
-  }
+const reverseArray = (arr) => {
+  console.log(arr.reverse())
 }
-console.log(largest)
+
+console.log(reverseArray(['a', 'b', 'c']))

0383f16eb3570072c42f1cc3a8790cb6f07cc6aa
Rahul <r27m93@gmail.com>
2022-02-04 12:07:27 +0530

    triangle solution alternate'

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-// const triangle = (num) => {
-//   let triangle = []
-//   let line = ''
-//   do {
-//     // console.log(line)
-//     line = line + '#'
-//     triangle.push(line)
-//   } while (line.length < num)
+const triangle = (num) => {
+  let triangle = []
+  let line = ''
+  do {
+    // console.log(line)
+    line = line + '#'
+    triangle.push(line)
+  } while (line.length < num)
 
-//   return triangle
-// }
+  return triangle
+}
 
-// console.log(triangle(8))
+console.log(triangle(8))

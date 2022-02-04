8e225e6f9d81bf83f9d2c3bd620dfaf908e63f1c
Rahul <r27m93@gmail.com>
2022-02-04 12:06:07 +0530

    fizzbuzz

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-'use strict'
-let line
+// const triangle = (num) => {
+//   let triangle = []
+//   let line = ''
+//   do {
+//     // console.log(line)
+//     line = line + '#'
+//     triangle.push(line)
+//   } while (line.length < num)
 
-for (let line = '#'; line.length < 8; line += '#') console.log(line)
+//   return triangle
+// }
+
+// console.log(triangle(8))

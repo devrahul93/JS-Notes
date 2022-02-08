f75d57fe7de14cb1357c775d7a2347cc059b98c6
Rahul <r27m93@gmail.com>
2022-02-08 16:14:28 +0530

    next done

--- README.md
+++ README.md
@@ -1149,4 +1149,27 @@ console.log(removeMiddleItem([1, 2, 3], 4, 5, 6))
 ```
 
 </li>
+<li>
+Create a function that takes two strings and returns true if the first argument ends with the second argument; otherewise return false . Take two strings as arguments. Determine if second string matches ending of first string. Return boolean value. Example
+
+yourFunc("integrity", "ity")
+// true
+yourFunc("integration", "tio")
+// false
+yourFunc("connection", "sion")
+// false
+yourFunc("connection", "tion")
+// true
+
+```Js
+const yourFunc = (word, endWrd) => {
+  let result = word.endsWith(endWrd)
+  console.log(result)
+}
+
+console.log(yourFunc('connection', 'tion'))
+
+```
+
+</li>
 </ol>
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-// Write a function which removes items from the middle of an array and replace with three items.
+// Create a function that takes two strings and returns true if the first argument ends with the second argument; otherewise return false . Take two strings as arguments. Determine if second string matches ending of first string. Return boolean value. Example
 
-// removeMiddleItem([1,2,3], 4,5,6)
-// [1,4,5,6,3]
-// removeMiddleItem([1,2,3,4], 4,5,6)
-//  [1,4,5,6,4]
+// yourFunc("integrity", "ity")
+// true
+// yourFunc("integration", "tio")
+// false
+// yourFunc("connection", "sion")
+// false
+// yourFunc("connection", "tion")
+// true
 
-const removeMiddleItem = (arr, ...value) => {
-  return [arr[0], ...value, arr[arr.length - 1]]
+const yourFunc = (word, endWrd) => {
+  let result = word.endsWith(endWrd)
+  console.log(result)
 }
-console.log(removeMiddleItem([1, 2, 3], 4, 5, 6))
 
-removeMiddleItem([1, 2, 3, 4], 4, 5, 6)
+console.log(yourFunc('connection', 'tion'))

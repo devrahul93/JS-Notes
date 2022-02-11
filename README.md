08655e1890f90ac10e0145a7dc6805353d5529e8
Rahul <r27m93@gmail.com>
2022-02-11 15:44:57 +0530

    add task complete

--- README.md
+++ README.md
@@ -1371,4 +1371,39 @@ removeProduct(3)
 
 </li>
 
+<li>
+The following todoList has three tasks. Create an addTask, removeTask, editTask, toggleTask, toggleAll, removeAll functions to modify the todoList.
+
+const todoList = [
+{
+task:'Prepare JS Test',
+time:'4/3/2019 8:30',
+completed:true
+},
+{
+task:'Give JS Test',
+time:'4/3/2019 10:00',
+completed:false
+},
+{
+task:'Assess Test Result',
+time:'4/3/2019 1:00',
+completed:false
+}]
+
+```Js
+const addTask = (task, time, complete) => {
+  const entries = new Map([
+    ['task', task],
+    ['time', time],
+    ['completed', complete],
+  ])
+
+  const obj = Object.fromEntries(entries)
+  todoList.push(obj)
+  return todoList
+}
+```
+
+</li>
 </ol>
--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-//The following shopping cart has four products. Create an addProduct, removeProduct ,editProduct , removeAll functions to modify the shopping cart.
+//The following todoList has three tasks. Create an addTask, removeTask, editTask, toggleTask, toggleAll, removeAll functions to modify the todoList.
 
-const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+const todoList = [
+  {
+    task: 'Prepare JS Test',
+    time: '4/3/2019 8:30',
+    completed: true,
+  },
+  {
+    task: 'Give JS Test',
+    time: '4/3/2019 10:00',
+    completed: false,
+  },
+  {
+    task: 'Assess Test Result',
+    time: '4/3/2019 1:00',
+    completed: false,
+  },
+]
 
-function addProduct(product) {
-  shoppingCart.push(product)
-  return shoppingCart
+const editTask = (taskNum, newTask, newTime, bool) => {
+  todoList[taskNum].task = newTask
+  todoList[taskNum].time = newTime
+  todoList[taskNum].completed = bool
+  return todoList
 }
 
-addProduct('Meat')
-// ["Milk", "Coffee", "Tea", "Honey", "Meat"]
-
-function editProduct(position, product) {
-  shoppingCart.splice(position, 1, product)
-  return shoppingCart
-}
-
-editProduct(3, 'Sugar')
-// ["Milk", "Coffee", "Tea", "Sugar", "Meat"]
-
-function removeProduct(num) {
-  shoppingCart.splice(num, 1)
-  return shoppingCart
-}
-
-removeProduct(0)
-// ["Coffee", "Tea", "Sugar", "Meat"]
-
-removeProduct(3)
-// ["Coffee", "Tea", "Sugar"]
+console.log(editTask(0, 'Complete Js Test', '11-02-2022 12.00', false))

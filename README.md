912b0cbdacad68bae42badcc09605000534f816a
Rahul <r27m93@gmail.com>
2022-02-11 19:32:46 +0530

    added score greater than

--- README.md
+++ README.md
@@ -1444,4 +1444,68 @@ console.log(checkUniqueness(arrTwo))
 ```
 
 </li>
+Write a function which filter users who has scoresGreaterThan85, Write a function which addUser to the user array only if the user does not exist. Write a function which addUserSkill which can add skill to a user only if the user exist. Write a function which editUser if the user exist in the users array.
+
+const users = [
+{
+name:'Brook',
+scores:75,
+skills:['HTM', 'CSS', 'JS'],
+age:16
+},
+{
+name:'Alex',
+scores:80,
+skills:['HTM', 'CSS', 'JS'],
+age:18
+},
+{
+name:'David',
+scores:75,
+skills:['HTM', 'CSS'],
+age:22
+},
+{
+name:'John',
+scores:85,
+skills:['HTM'],
+age:25
+},
+{
+name:'Sara',
+scores:95,
+skills:['HTM', 'CSS', 'JS'],
+age: 26
+},
+{
+name:'Martha',
+scores:80,
+skills:['HTM', 'CSS', 'JS'],
+age:18
+},
+{
+name:'Thomas',
+scores:90,
+skills:['HTM', 'CSS', 'JS'],
+age:20
+}
+];
+
+```Js
+
+function scoresGreaterThan85(user) {
+  let newArr = []
+  for (let i = 0; i < user.length; i++) {
+    if (user[i].scores > 85) {
+      newArr.push(user[i])
+    }
+  }
+  console.log(new Arr())
+}
+
+scoresGreaterThan85(users)
+```
+
+<li>
+</li>
 </ol>
--- index.js
+++ index.js
@@ -1,22 +1,45 @@
-//Write a function which check if items of an array are unique?
-
-const arrOne = [1, 4, 6, 2, 1]
-const arrTwo = [1, 4, 6, 2, 3]
-
-function checkUniqueness(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      if (i != j) {
-        if (arr[i] == arr[j]) {
-          return true
-        }
-      }
-    }
-  }
-  return false
-}
-
-console.log(checkUniqueness(arrOne))
-//false
-console.log(checkUniqueness(arrTwo))
-//true
+//Write a function which filter users who has scoresGreaterThan85, Write a function which addUser to the user array only if the user does not exist. Write a function which addUserSkill which can add skill to a user only if the user exist. Write a function which editUser if the user exist in the users array.
+const users = [
+  {
+    name: 'Brook',
+    scores: 75,
+    skills: ['HTM', 'CSS', 'JS'],
+    age: 16,
+  },
+  {
+    name: 'Alex',
+    scores: 80,
+    skills: ['HTM', 'CSS', 'JS'],
+    age: 18,
+  },
+  {
+    name: 'David',
+    scores: 75,
+    skills: ['HTM', 'CSS'],
+    age: 22,
+  },
+  {
+    name: 'John',
+    scores: 85,
+    skills: ['HTM'],
+    age: 25,
+  },
+  {
+    name: 'Sara',
+    scores: 95,
+    skills: ['HTM', 'CSS', 'JS'],
+    age: 26,
+  },
+  {
+    name: 'Martha',
+    scores: 80,
+    skills: ['HTM', 'CSS', 'JS'],
+    age: 18,
+  },
+  {
+    name: 'Thomas',
+    scores: 90,
+    skills: ['HTM', 'CSS', 'JS'],
+    age: 20,
+  },
+]

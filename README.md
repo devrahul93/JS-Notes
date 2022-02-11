42272baba3c74a6b8192f2f184eee5bcf54767a8
Rahul <r27m93@gmail.com>
2022-02-11 23:46:32 +0530

    square and cube done

--- README.md
+++ README.md
@@ -1607,6 +1607,31 @@ num.forEach((element) =>
 ```
 
 </li>
+<li>
+Print the following pattern using loop and console.log(). (2.5 pts)
+
+```Md
+i    i^2   i^3
+0    0     0
+1    1     1
+2    4     8
+3    9     27
+4    16    64
+5    25    125
+6    36    216
+7    49    343
+8    64    512
+9    81    729
+10   100   1000
+```
+
+```Js
+for (let index = 0; index < 10; index++) {
+  console.log(`${index}   ${index * index}   ${index * index * index}`)
+}
+```
+
+</li>
 </ol>
 </li>
 </ol>
--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-//Print the following pattern using loop and console.log(). (2.5 pts)
+// Print the following pattern using loop and console.log(). (2.5 pts)
 
-// 0 x 0 = 0
-// 1 x 1 = 1
-// 2 x 2 = 4
-// 3 x 3 = 9
-// 4 x 4 = 16
-// 5 x 5 = 25
-// 6 x 6 = 36
-// 7 x 7 = 49
-// 8 x 8 = 64
-// 9 x 9 = 81
-// 10 x 10 = 100
+// i    i^2   i^3
+// 0    0     0
+// 1    1     1
+// 2    4     8
+// 3    9     27
+// 4    16    64
+// 5    25    125
+// 6    36    216
+// 7    49    343
+// 8    64    512
+// 9    81    729
+// 10   100   1000
+
+for (let index = 0; index < 10; index++) {
+  console.log(`${index}   ${index * index}   ${index * index * index}`)
+}
